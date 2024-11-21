284f4e113d289a4ccf4540a7d6bf4e3faf320ad7
Ailton_78 <sd.ailtonsampaio@gmail.com>
2024-11-21 11:56:46 -0300

    create project

---
+++ src/test/java/com/githunb/ailton78/desafio_orm/DesafioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githunb.ailton78.desafio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
